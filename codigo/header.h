c2b4033ef775eb2712ceb7ecaa0a253c055bc5df
Vanessa Montenegro <vanessa.montenegro@udea.edu.co>
2025-04-24 18:42:12 -0500

    Legibilidad de parametros en header

--- codigo/header.h
+++ codigo/header.h
@@ -11,52 +11,25 @@ bool exportImage(unsigned char* pixelData, int width,int height, QString archivo
 
 unsigned int* loadSeedMasking(const char* nombreArchivo, int &seed, int &n_pixels);
 
-void desplazamientoDerecha(unsigned char *array,
-                           int semilla,
-                           unsigned short int *arrayCopia,
-                           int bytesMascara,
-                           unsigned char numDesplazamiento);
+void desplazamientoDerecha(unsigned char *array, int semilla, unsigned short int *arrayCopia, int bytesMascara, unsigned char numDesplazamiento);
 
-void desplazamientoIzquierda(unsigned char *array,
-                             int semilla,
-                             unsigned short int *arrayCopia,
-                             int bytesMascara,
-                             unsigned char numDesplazamiento);
+void desplazamientoIzquierda(unsigned char *array, int semilla, unsigned short int *arrayCopia, int bytesMascara, unsigned char numDesplazamiento);
 
-void rotacionDerecha(unsigned char *array,
-                     int semilla,
-                     unsigned short int *arrayCopia,
-                     int bytesMascara,
-                     unsigned char numRotacion);
+void rotacionDerecha(unsigned char *array, int semilla, unsigned short int *arrayCopia, int bytesMascara, unsigned char numRotacion);
 
-void rotacionIzquierda(unsigned char *array,
-                       int semilla,
-                       unsigned short int *arrayCopia,
-                       int bytesMascara,
-                       unsigned char numRotacion);
+void rotacionIzquierda(unsigned char *array, int semilla, unsigned short int *arrayCopia, int bytesMascara, unsigned char numRotacion);
 
-void xorEntreImagenes(unsigned short int *arrayCopia,
-                      int semilla,
-                      unsigned char *arrayI_M,
-                      int bytesMascara);
+void xorEntreImagenes(unsigned short int *arrayCopia, int semilla, unsigned char *arrayI_M, int bytesMascara);
 
 bool comparar(unsigned int *arrayTexto, unsigned short int *arrayCopia);
 
-void desplazamientoDerechaImagen(unsigned char *arrayImagen,
-                                 int numBytesImagen,
-                                 unsigned char numDesplazamiento);
+void desplazamientoDerechaImagen(unsigned char *arrayImagen, int numBytesImagen, unsigned char numDesplazamiento);
 
-void desplazamientoIzquierdaImagen(unsigned char *arrayImagen,
-                                   int numBytesImagen,
-                                   unsigned char numDesplazamiento);
+void desplazamientoIzquierdaImagen(unsigned char *arrayImagen, int numBytesImagen, unsigned char numDesplazamiento);
 
-void rotacionDerechaImagen(unsigned char *arrayImagen,
-                           int numBytesImagen,
-                           unsigned char numRotacionn);
+void rotacionDerechaImagen(unsigned char *arrayImagen, int numBytesImagen, unsigned char numRotacionn);
 
-void rotacionIzquierdaImagen(unsigned char *arrayImagen,
-                             int numBytesImagen,
-                             unsigned char numRotacionn);
+void rotacionIzquierdaImagen(unsigned char *arrayImagen, int numBytesImagen, unsigned char numRotacionn);
 
 void xorImagenCompleta(unsigned char *arrayImagen, int numBytesImagen, unsigned char *arrayI_M);
 
