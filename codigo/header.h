00dc3cc4534f25a61b90d6a9b1a262e3c0e517d8
Vanessa Montenegro <vanessa.montenegro@udea.edu.co>
2025-04-25 13:14:59 -0500

    Eliminacion del modulo funcones_transformaciones_general

--- codigo/header.h
+++ codigo/header.h
@@ -3,6 +3,7 @@
 
 #include <QCoreApplication>
 
+//Funciones archivos
 unsigned short int *crearArrayCopia(unsigned char *arrayOriginal, int semilla, int numBytesMascara);
 
 unsigned char* loadPixels(QString input, int &width, int &height);
@@ -11,6 +12,8 @@ bool exportImage(unsigned char* pixelData, int width,int height, QString archivo
 
 unsigned int* loadSeedMasking(const char* nombreArchivo, int &seed, int &n_pixels);
 
+
+//Funciones transformaciones
 void desplazaminetoDerecha(unsigned char *arrayImagen = nullptr, int semilla = 0, unsigned short int *arrayCopia = nullptr, int nBytes = 0, unsigned char numDesplazamiento = 0);
 
 void desplazamientoIzquierda(unsigned char *arrayImagen = nullptr, int semilla = 0, unsigned short int *arrayCopia = nullptr, int nBytes = 0, unsigned char numDesplazamiento = 0);
@@ -21,6 +24,8 @@ void rotacionIzquierda(unsigned char *arrayImagen = nullptr, int semilla = 0, un
 
 void xorEntreImagenes(unsigned char* arrayImagen = nullptr, unsigned short int *arrayCopia = nullptr, int semilla = 0, unsigned char *arrayI_M = nullptr, int nBytes = 0);
 
+
+//Funciones comparar
 bool comparar(unsigned int *arrayTexto, unsigned short int *arrayCopia);
 
 void sumarMascara(unsigned short int *arrayCopia, unsigned char *arrayMascara, int numBytesMascara);
