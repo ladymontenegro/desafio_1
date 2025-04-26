5d3b478ba04f4d2657d23a3b1954da6fc93a6e76
Vanessa Montenegro <vanessa.montenegro@udea.edu.co>
2025-04-25 19:53:05 -0500

    Modificacion docstring a funciones transformaciones

--- codigo/funciones_transformaciones.cpp
+++ codigo/funciones_transformaciones.cpp
@@ -7,17 +7,17 @@ using namespace std;
 void desplazamientoDerecha(unsigned char *arrayImagen, int semilla, unsigned short int *arrayCopia, int nBytes, unsigned char numDesplazamiento)
 {
     /*
- * @brief Realiza un desplazamiento a la derecha de los bits de una porcion de un arreglo.
+ * @brief Realiza un desplazamiento a la derecha de los bits de una porcion de un arreglo de imagen o de todo el arreglo.
  *
- * Esta funcion itera a traves de una porcion del arreglo 'array' de entrada, comenzando
- * en la posicion indicada por `semilla` y procesando 'bytesMascara' bytes. Para cada byte
- * leido, aplica un desplazamiento a la derecha (operador >>) por el numero de bits
- * especificado en 'numDesplazamiento', y guarda el resultado en el arreglo 'arrayCopia'.
+ * Esta funcion aplica un desplazamiento a la derecha (operador >>) por el numero de bits especificado en 'numDesplazamiento'.
+ * Si 'semilla' es diferente de cero, la operacion se realiza sobre 'nBytes' bytes del 'arrayImagen', comenzando desde el
+ * indice 'semilla', y los resultados se guardan en el 'arrayCopia'. Cada byte desplazado se expande a un 'unsigned short int'.
+ * Si 'semilla' es cero, la operacion se realiza directamente sobre los primeros 'nBytes' del 'arrayImagen'.
  *
- * @param array Puntero al arreglo de unsigned char de origen.
- * @param semilla Indice de inicio en el arreglo 'array' para la lectura de bytes.
- * @param arrayCopia Puntero al arreglo de unsigned short int donde se guardaran los resultados del desplazamiento.
- * @param bytesMascara Numero de bytes a procesar desde el arreglo 'array'.
+ * @param arrayImagen Puntero al arreglo de unsigned char que contiene los datos de la imagen.
+ * @param semilla Indice de inicio en 'arrayImagen' para la lectura (si no es 0). Si es 0, se opera directamente sobre el inicio del arreglo.
+ * @param arrayCopia Puntero al arreglo de unsigned short int donde se guardaran los resultados del desplazamiento (solo si semilla != 0).
+ * @param nBytes Numero de bytes a procesar.
  * @param numDesplazamiento Numero de bits a desplazar hacia la derecha (tipo unsigned char).
  */
     if (semilla != 0){
@@ -34,18 +34,18 @@ void desplazamientoDerecha(unsigned char *arrayImagen, int semilla, unsigned sho
 void desplazamientoIzquierda(unsigned char *arrayImagen, int semilla, unsigned short int *arrayCopia, int nBytes, unsigned char numDesplazamiento)
 {
     /*
- * @brief Realiza un desplazamiento a la izquierda de los bits de una porcion de un arreglo.
+ * @brief Realiza un desplazamiento a la derecha de los bits de una porcion de un arreglo de imagen o de todo el arreglo.
  *
- * Esta funcion recorre una seccion del arreglo 'array' de entrada, empezando
- * en el indice 'semilla' y trabajando con 'bytesMascara' bytes. Por cada byte
- * extraido, efectua un desplazamiento a la izquierda (operador <<) por la cantidad de bits
- * indicada en 'numDesplazamiento', y almacena el valor resultante en el arreglo 'arrayCopia'.
+ * Esta funcion aplica un desplazamiento a la izquierda (operador <<) por el numero de bits especificado en 'numDesplazamiento'.
+ * Si 'semilla' es diferente de cero, la operacion se realiza sobre 'nBytes' bytes del 'arrayImagen', comenzando desde el
+ * indice 'semilla', y los resultados se guardan en el 'arrayCopia'. Cada byte desplazado se expande a un 'unsigned short int'.
+ * Si 'semilla' es cero, la operacion se realiza directamente sobre los primeros 'nBytes' del 'arrayImagen'.
  *
- * @param array Puntero al arreglo de unsigned char de donde se leen los datos.
- * @param semilla Indice inicial en el arreglo 'array' para la lectura de los bytes.
- * @param arrayCopia Puntero al arreglo de unsigned short int donde se escriben los bytes desplazados.
- * @param bytesMascara Cantidad de bytes a procesar del arreglo 'array'.
- * @param numDesplazamiento Numero de bits a desplazar hacia la izquierda (tipo unsigned char).
+ * @param arrayImagen Puntero al arreglo de unsigned char que contiene los datos de la imagen.
+ * @param semilla Indice de inicio en 'arrayImagen' para la lectura (si no es 0). Si es 0, se opera directamente sobre el inicio del arreglo.
+ * @param arrayCopia Puntero al arreglo de unsigned short int donde se guardaran los resultados del desplazamiento (solo si semilla != 0).
+ * @param nBytes Numero de bytes a procesar.
+ * @param numDesplazamiento Numero de bits a desplazar hacia la derecha (tipo unsigned char).
  */
     if (semilla != 0){
         for (int i = 0; i < nBytes; semilla ++, i ++) {
@@ -61,20 +61,19 @@ void desplazamientoIzquierda(unsigned char *arrayImagen, int semilla, unsigned s
 void rotacionDerecha(unsigned char *arrayImagen, int semilla, unsigned short int *arrayCopia, int nBytes, unsigned char numRotacion)
 {
     /*
- * @brief Realiza una rotacion a la derecha de los bits de una porcion de un arreglo.
+ * @brief Realiza una rotacion a la derecha de los bits de una porcion de un arreglo de imagen o de todo el arreglo.
  *
- * Esta funcion procesa 'bytesMascara' bytes del arreglo 'array' comenzando desde el indice 'semilla'.
- * Para cada byte, realiza una rotacion circular a la derecha por 'numRotacion' bits.
- * Los bits que salen por la derecha vuelven a entrar por la izquierda.
- * El resultado de cada rotacion se guarda en el arreglo 'arrayCopia'.
+ * Esta funcion efectua una rotacion circular a la derecha por 'numRotacion' bits.
+ * Si 'semilla' es diferente de cero, se rotan 'nBytes' bytes de 'arrayImagen' a partir del indice 'semilla',
+ * y los resultados se guardan en 'arrayCopia' como 'unsigned short int'.
+ * Si 'semilla' es cero, la rotacion se aplica directamente a los primeros 'nBytes' de 'arrayImagen'.
  *
- * @param array Puntero al arreglo de unsigned char de entrada.
- * @param semilla Indice de inicio en el arreglo 'array'.
- * @param arrayCopia Puntero al arreglo de unsigned short int donde se almacenan los resultados de la rotacion.
- * @param bytesMascara Numero de bytes a rotar.
+ * @param arrayImagen Puntero al arreglo de unsigned char que contiene los datos de la imagen.
+ * @param semilla Indice de inicio en 'arrayImagen' para la lectura (si no es 0). Si es 0, se opera directamente sobre el inicio del arreglo.
+ * @param arrayCopia Puntero al arreglo de unsigned short int donde se guardaran los resultados de la rotacion (solo si semilla != 0).
+ * @param nBytes Numero de bytes a rotar.
  * @param numRotacion Numero de bits a rotar hacia la derecha (tipo unsigned char).
- */
-
+ **/
     if (semilla !=0){
         for (int i = 0; i < nBytes; semilla ++, i ++) {
             unsigned char byte = *(arrayImagen + semilla);
@@ -96,19 +95,19 @@ void rotacionDerecha(unsigned char *arrayImagen, int semilla, unsigned short int
 void rotacionIzquierda(unsigned char *arrayImagen, int semilla, unsigned short int *arrayCopia, int nBytes, unsigned char numRotacion)
 {
     /*
- * @brief Realiza una rotacion a la izquierda de los bits de una porcion de un arreglo.
+ * @brief Realiza una rotacion a la izquierda de los bits de una porcion de un arreglo de imagen o de todo el arreglo.
  *
- * Esta funcion itera a traves de 'bytesMascara' bytes del arreglo 'array', empezando en 'semilla'.
- * Para cada byte, efectua una rotacion circular a la izquierda por 'numRotacion' bits.
- * Los bits que se desplazan fuera de la izquierda reaparecen por la derecha.
- * El byte rotado se guarda en el arreglo 'arrayCopia'.
+ * Esta funcion efectua una rotacion circular a la derecha por 'numRotacion' bits.
+ * Si 'semilla' es diferente de cero, se rotan 'nBytes' bytes de 'arrayImagen' a partir del indice 'semilla',
+ * y los resultados se guardan en 'arrayCopia' como 'unsigned short int'.
+ * Si 'semilla' es cero, la rotacion se aplica directamente a los primeros 'nBytes' de 'arrayImagen'.
  *
- * @param array Puntero al arreglo de unsigned char de origen.
- * @param semilla Indice de inicio para la lectura en 'array'.
- * @param arrayCopia Puntero al arreglo de unsigned short int donde se escriben los bytes rotados.
- * @param bytesMascara Cantidad de bytes a rotar.
- * @param numRotacion Numero de bits a rotar hacia la izquierda (tipo unsigned char).
- */
+ * @param arrayImagen Puntero al arreglo de unsigned char que contiene los datos de la imagen.
+ * @param semilla Indice de inicio en 'arrayImagen' para la lectura (si no es 0). Si es 0, se opera directamente sobre el inicio del arreglo.
+ * @param arrayCopia Puntero al arreglo de unsigned short int donde se guardaran los resultados de la rotacion (solo si semilla != 0).
+ * @param nBytes Numero de bytes a rotar.
+ * @param numRotacion Numero de bits a rotar hacia la derecha (tipo unsigned char).
+ **/
     if (semilla != 0){
         for (int i = 0; i < nBytes; semilla ++, i ++) {
             unsigned char byte = *(arrayImagen + semilla);
@@ -130,17 +129,18 @@ void rotacionIzquierda(unsigned char *arrayImagen, int semilla, unsigned short i
 void xorEntreImagenes(unsigned char* arrayImagen, unsigned short int *arrayCopia, int semilla, unsigned char *arrayI_M, int nBytes)
 {
     /*
- * @brief Aplica una operacion XOR bit a bit entre una porcion de un arreglo y otra.
+ * @brief Aplica una operacion XOR bit a bit entre una porcion de un arreglo de imagen y otra, o entre arreglos completos.
  *
- * Esta funcion compara 'bytesMascara' elementos del 'arrayCopia', comenzando desde el indice 0,
- * con elementos del 'arrayI_M' empezando desde el indice 'semilla'. Realiza una operacion XOR
- * (operador ^) entre los elementos correspondientes y guarda el resultado en la posicion
- * del 'arrayCopia'.
+ * Si 'semilla' no es cero, esta funcion realiza una operacion XOR (operador ^) entre 'nBytes' elementos del 'arrayCopia' (comenzando desde el indice 0)
+ * y 'nBytes' elementos del 'arrayI_M' (comenzando desde el indice 'semilla'). Los resultados se guardan en 'arrayCopia'.
+ * Si 'semilla' es cero, la operacion XOR se realiza entre los primeros 'nBytes' de 'arrayImagen' y los primeros 'nBytes' de 'arrayI_M',
+ * y los resultados se guardan en 'arrayImagen'.
  *
- * @param arrayCopia Puntero al arreglo de unsigned short int que se modificara con el resultado del XOR.
- * @param semilla Indice de inicio en el arreglo 'arrayI_M' para la lectura de los bytes.
+ * @param arrayImagen Puntero al arreglo de unsigned char de la imagen (modificado si semilla es 0).
+ * @param arrayCopia Puntero al arreglo de unsigned short int donde se guardaran los resultados del XOR (si semilla no es 0).
+ * @param semilla Indice de inicio en 'arrayI_M' para la lectura (si no es 0). Si es 0, la operacion se realiza desde el inicio de los arreglos.
  * @param arrayI_M Puntero al arreglo de unsigned char con el que se realizara la operacion XOR.
- * @param bytesMascara Numero de elementos a procesar de ambos arreglos.
+ * @param nBytes Numero de elementos a procesar de ambos arreglos.
  */
     if (semilla != 0){
         for (int i = 0; i < nBytes; semilla++, i++) {
